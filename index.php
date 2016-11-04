8a9e9f6dd948bdee1c9c36a10d7c2fa17317326b
UltimateSky <chas.rivers1001@gmail.com>
2016-11-04 00:05:22 -0400

    Updated index and added register.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" ng-app="project">
+<html lang="en">
 <head>
     <meta charset="utf-8">
 
@@ -51,7 +51,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">coaster.io</a>
+        <a class="navbar-brand" href="/index.php">coaster.io</a>
     </div>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -69,7 +69,7 @@
             <hr>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Sign Up</a></li>
+            <li><a href="/register.php">Sign Up</a></li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sign in <b class="caret"></b></a>
                 <ul class="dropdown-menu" style="padding: 15px;min-width: 250px;">
@@ -79,7 +79,7 @@
                                 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
                                     <div class="form-group">
                                         <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail2" style="width: 100%" placeholder="Email address" required>
+                                        <input type="email" class="form-control" id="exampleInputEmail2" style="width: 100%" placeholder="Username" required>
                                     </div>
                                     <div class="form-group">
                                         <label class="sr-only" for="exampleInputPassword2">Password</label>
@@ -158,13 +158,48 @@
                             </div>
                         </div>
                     </div> <!-- / example card -->
-                    <ul class="list-group">
-                        <li class="list-group-item" id="selected">This</li>
-                        <li class="list-group-item" id="selected">is</li>
-                        <li class="list-group-item" id="selected">filler</li>
-                        <li class="list-group-item" id="selected">random</li>
-                        <li class="list-group-item" id="selected">stuff</li>
-                    </ul>
+                    <div class="col-lg-5 col-md-5 col-sm-8 col-xs-9 bhoechie-tab-container">
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 bhoechie-tab-menu">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item active text-center item-width">
+                                    <h4 class="glyphicon glyphicon-glass"></h4><br/>Drink
+                                </a>
+                                <a href="#" class="list-group-item text-center item-width">
+                                    <h4 class="glyphicon glyphicon-list-alt"></h4><br/>Ingredients
+                                </a>
+                                <a href="#" class="list-group-item text-center item-width">
+                                    <h4 class="glyphicon glyphicon-pencil"></h4><br/>Directions
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 bhoechie-tab">
+                            <!-- flight section -->
+                            <div class="bhoechie-tab-content active">
+                                <center>
+                                    <h1 class="glyphicon glyphicon-glass glyph-size" style="color:#55518a"></h1>
+                                    <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
+                                    <h3 style="margin-top: 0;color:#55518a">Drink Name</h3>
+                                </center>
+                            </div>
+                            <!-- train section -->
+                            <div class="bhoechie-tab-content">
+                                <center>
+                                    <h1 class="glyphicon glyphicon-list-alt glyph-size" style="color:#55518a"></h1>
+                                    <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
+                                    <h3 style="margin-top: 0;color:#55518a">1. Ingredient 1</h3>
+                                </center>
+                            </div>
+
+                            <!-- hotel search -->
+                            <div class="bhoechie-tab-content">
+                                <center>
+                                    <h1 class="glyphicon glyphicon-pencil glyph-size" style="color:#55518a"></h1>
+                                    <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
+                                    <h3 style="margin-top: 0;color:#55518a">1. Step 1</h3>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
@@ -210,6 +245,17 @@
         } else {
             $('nav').removeClass('shrink');
         }
+    });
+
+    $(document).ready(function() {
+        $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+            e.preventDefault();
+            $(this).siblings('a.active').removeClass("active");
+            $(this).addClass("active");
+            var index = $(this).index();
+            $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+            $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+        });
     });
 </script>
 </body>
---
+++ register.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <!-- Bootstrap stuffs -->
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="static/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="static/css/register.css">
+
+    <!-- JQuery -->
+    <script src="/static/js/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="/static/js/bootstrap.min.js"></script>
+
+    <script src="/static/js/modal.js"></script>
+
+    <title>coaster.io | Registration</title>
+</head>
+<body>
+<!-- Nav Bar -->
+<nav class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="100" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">coaster.io</a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li class="dropdown hidden-lg">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sections <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#left">Search</a></li>
+                    <li><a href="#center">Drinks</a></li>
+                    <li><a href="#right">List</a></li>
+                </ul>
+            </li>
+            <hr>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="/register.php">Sign Up</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sign in <b class="caret"></b></a>
+                <ul class="dropdown-menu" style="padding: 15px;min-width: 250px;">
+                    <li>
+                        <div class="row">
+                            <div class="col-md-12" id="login-modal">
+                                <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                        <input type="email" class="form-control logcolor" id="exampleInputEmail2" style="width: 100%" placeholder="Email address" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                        <input type="password" class="form-control logcolor" id="exampleInputPassword2" style="width: 100%" placeholder="Password" required>
+                                    </div>
+                                    <div class="checkbox wht">
+                                        <label>
+                                            <input type="checkbox"> Remember me
+                                        </label>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-success btn-block">Sign in</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <!-- /.navbar-collapse -->
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-login">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <form id="login-form" action="#" method="post" role="form" style="display: block;">
+                                <h2>LOGIN</h2>
+                                <div class="form-group">
+                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" required="required">
+                                </div>
+                                <div class="col-xs-6 form-group pull-left checkbox">
+                                    <input id="checkbox1" type="checkbox" name="remember">
+                                    <label for="checkbox1">Remember Me</label>
+                                </div>
+                                <div class="col-xs-6 form-group pull-right">
+                                    <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
+                                </div>
+                            </form>
+                            <form id="register-form" action="#" method="post" role="form" style="display: none;">
+                                <h2>REGISTER</h2>
+                                <div class="form-group">
+                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" required="required" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-sm-6 col-sm-offset-3">
+                                            <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-6 tabs">
+                            <a href="#" class="active" id="login-form-link"><div class="login">LOGIN</div></a>
+                        </div>
+                        <div class="col-xs-6 tabs">
+                            <a href="#" id="register-form-link"><div class="register">REGISTER</div></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(function() {
+        $('#login-form-link').click(function(e) {
+            $("#login-form").delay(100).fadeIn(100);
+            $("#register-form").fadeOut(100);
+            $('#register-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+        $('#register-form-link').click(function(e) {
+            $("#register-form").delay(100).fadeIn(100);
+            $("#login-form").fadeOut(100);
+            $('#login-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+
+    });
+</script>
+</body>
+</html>
