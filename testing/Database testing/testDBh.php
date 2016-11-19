2591ac0cb4eeae849fcbbc8e3f1ce6fbedcd7a6f
td02836 <td02836@georgiasouthern.edu>
2016-11-19 17:48:03 -0500

    Added functionality testDBh
    Added all mixers and garnishes to echo display

--- testing/Database testing/testDBh.php
+++ testing/Database testing/testDBh.php
@@ -14,8 +14,14 @@ if ($conn->connect_error) {
 }
 else {
 	$sql = "SELECT Liquor_Name FROM liquors";
-	$result = $conn->query($sql);
-
+	$sql2 = "SELECT Mixer_Name FROM Mixers";
+	$sql3 = "SELECT Garnish_Name FROM Garnishes";
+	
+	$result = $conn->query($sql); // liquors
+	$result2 = $conn->query($sql2); // mixers
+	$result3 = $conn->query($sql3); // garnishes
+	
+	// Liquors
 	$response=array();
 	if ($result->num_rows > 0) {
 		// output data of each row
@@ -28,6 +34,35 @@ else {
 		$jsonData=json_encode(array_values((array)$response[$i]));
 		echo $jsonData;
 	}
+	
+	// Mixers
+	$response2=array();
+	if ($result2->num_rows > 0) {
+		// output data of each row
+		while($row = $result2->fetch_assoc()) {
+			$response2[]=$row;
+		}
+	}
+	
+	for ($i = 0; $i < $result2->num_rows; ++$i) {
+		$jsonData2=json_encode(array_values((array)$response2[$i]));
+		echo $jsonData2;
+	}
+	
+	// Garnishes
+	$response3=array();
+	if ($result3->num_rows > 0) {
+		// output data of each row
+		while($row = $result3->fetch_assoc()) {
+			$response3[]=$row;
+		}
+	}
+	
+	for ($i = 0; $i < $result3->num_rows; ++$i) {
+		$jsonData3=json_encode(array_values((array)$response3[$i]));
+		echo $jsonData3;
+	}
+	
 
 }
 $conn->close();
