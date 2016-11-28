583ecde3692b14c09bcbc5e8cd11cb78bfd06471
ahong211 <ahong211@gmail.com>
2016-11-28 09:53:35 -0500

    Added placeholder images in modals and removed unnecessary text inside

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="static/css/style.css?version=52">
     <link rel="stylesheet" href="static/css/carousel.css?version=50">
 
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
 
 <!-- Custom fonts -->
 <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -147,7 +147,7 @@
                         <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true" onclick="display();"></span></button>
                     </div>
                 </div>
-				
+
             </div>
 			<!-- Tyrone -->
 				<br/>
@@ -363,16 +363,16 @@
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
                                 <!-- Project Details Go Here -->
-                                <h2>Project Name</h2>
-                                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img class="img-responsive img-centered" src="img/portfolio/roundicons-free.png" alt="">
-                                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                <p>
-                                    <strong>Want these icons in this portfolio item sample?</strong>You can download 60 of them for free, courtesy of <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">RoundIcons.com</a>, or you can purchase the 1500 icon set <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">here</a>.</p>
+                                <h2>Whiskey Drinks</h2>
+                                <p class="item-intro text-muted">Rugged and hard-working</p>
+
                                 <ul class="list-inline">
-                                    <li>Date: July 2014</li>
-                                    <li>Client: Round Icons</li>
-                                    <li>Category: Graphic Design</li>
+                                    <li><img src="Images/Whiskey.jpg" class="img-responsive" alt="">
+                                    <p>Whiskey Coke</p></li>
+                                    <li><img src="Images/Whiskey.jpg" class="img-responsive" alt="">
+                                    <p>Whiskey Sour</p></li>
+                                    <li><img src="Images/Whiskey.jpg" class="img-responsive" alt="">
+                                    <p>On The Rocks</p></li>
                                 </ul>
                                 <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
                             </div>
@@ -397,11 +397,17 @@
                     <div class="row">
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
-                                <h2>Project Heading</h2>
-                                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img class="img-responsive img-centered" src="img/portfolio/startup-framework-preview.png" alt="">
-                                <p><a href="http://designmodo.com/startup/?u=787">Startup Framework</a> is a website builder for professionals. Startup Framework contains components and complex blocks (PSD+HTML Bootstrap themes and templates) which can easily be integrated into almost any design. All of these components are made in the same style, and can easily be integrated into projects, allowing you to create hundreds of solutions for your future projects.</p>
-                                <p>You can preview Startup Framework <a href="http://designmodo.com/startup/?u=787">here</a>.</p>
+                                <h2>Vodka Drinks</h2>
+                                <p class="item-intro text-muted">Staying classy</p>
+                                <ul class="list-inline">
+                                    <li><img src="Images/Vodka.jpg" class="img-responsive" alt="">
+                                    <p>Screwdriver</p></li>
+                                    <li><img src="Images/Vodka.jpg" class="img-responsive" alt="">
+                                    <p>Mojito</p></li>
+                                    <li><img src="Images/Vodka.jpg" class="img-responsive" alt="">
+                                    <p>Vodka + Juice</p></li>
+                                </ul>
+
                                 <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
                             </div>
                         </div>
@@ -426,11 +432,16 @@
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
                                 <!-- Project Details Go Here -->
-                                <h2>Project Name</h2>
-                                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img class="img-responsive img-centered" src="img/portfolio/treehouse-preview.png" alt="">
-                                <p>Treehouse is a free PSD web template built by <a href="https://www.behance.net/MathavanJaya">Mathavan Jaya</a>. This is bright and spacious design perfect for people or startup companies looking to showcase their apps or other projects.</p>
-                                <p>You can download the PSD template in this portfolio sample item at <a href="http://freebiesxpress.com/gallery/treehouse-free-psd-web-template/">FreebiesXpress.com</a>.</p>
+                                <h2>Gin Drinks</h2>
+                                <p class="item-intro text-muted">It's definitely not water</p>
+                                <ul class="list-inline">
+                                    <li><img src="Images/Gin.jpg" class="img-responsive" alt="">
+                                    <p>Gin and Tonic</p></li>
+                                    <li><img src="Images/Gin.jpg" class="img-responsive" alt="">
+                                    <p>Gin</p></li>
+                                    <li><img src="Images/Gin.jpg" class="img-responsive" alt="">
+                                    <p>Gin</p></li>
+                                </ul>
                                 <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
                             </div>
                         </div>
@@ -455,11 +466,16 @@
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
                                 <!-- Project Details Go Here -->
-                                <h2>Project Name</h2>
-                                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img class="img-responsive img-centered" src="img/portfolio/golden-preview.png" alt="">
-                                <p>Start Bootstrap's Agency theme is based on Golden, a free PSD website template built by <a href="https://www.behance.net/MathavanJaya">Mathavan Jaya</a>. Golden is a modern and clean one page web template that was made exclusively for Best PSD Freebies. This template has a great portfolio, timeline, and meet your team sections that can be easily modified to fit your needs.</p>
-                                <p>You can download the PSD template in this portfolio sample item at <a href="http://freebiesxpress.com/gallery/golden-free-one-page-web-template/">FreebiesXpress.com</a>.</p>
+                                <h2>Specialty Drinks</h2>
+                                <p class="item-intro text-muted">For teh adventurous types</p>
+                                <ul class="list-inline">
+                                    <li><img src="Images/OldFashioned.jpg" class="img-responsive" alt="">
+                                    <p>Old Fashioned</p></li>
+                                    <li><img src="Images/OldFashioned.jpg" class="img-responsive" alt="">
+                                    <p>Mixed Drink</p></li>
+                                    <li><img src="Images/OldFashioned.jpg" class="img-responsive" alt="">
+                                    <p>Mixed Drink</p></li>
+                                </ul>
                                 <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
                             </div>
                         </div>
@@ -484,11 +500,16 @@
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
                                 <!-- Project Details Go Here -->
-                                <h2>Project Name</h2>
-                                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img class="img-responsive img-centered" src="img/portfolio/escape-preview.png" alt="">
-                                <p>Escape is a free PSD web template built by <a href="https://www.behance.net/MathavanJaya">Mathavan Jaya</a>. Escape is a one page web template that was designed with agencies in mind. This template is ideal for those looking for a simple one page solution to describe your business and offer your services.</p>
-                                <p>You can download the PSD template in this portfolio sample item at <a href="http://freebiesxpress.com/gallery/escape-one-page-psd-web-template/">FreebiesXpress.com</a>.</p>
+                                <h2>Rum</h2>
+                                 <p class="item-intro text-muted">When you're feeling like a captain</p>
+                                <ul class="list-inline">
+                                    <li><img src="Images/Rum.jpg" class="img-responsive" alt="">
+                                    <p>Rum and Coke</p></li>
+                                    <li><img src="Images/Rum.jpg" class="img-responsive" alt="">
+                                    <p>Rum</p></li>
+                                    <li><img src="Images/Rum.jpg" class="img-responsive" alt="">
+                                    <p>Rum On The Rocks</p></li>
+                                </ul>
                                 <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
                             </div>
                         </div>
@@ -513,11 +534,16 @@
                         <div class="col-lg-8 col-lg-offset-2">
                             <div class="modal-body">
                                 <!-- Project Details Go Here -->
-                                <h2>Project Name</h2>
-                                <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img class="img-responsive img-centered" src="img/portfolio/dreams-preview.png" alt="">
-                                <p>Dreams is a free PSD web template built by <a href="https://www.behance.net/MathavanJaya">Mathavan Jaya</a>. Dreams is a modern one page web template designed for almost any purpose. It’s a beautiful template that’s designed with the Bootstrap framework in mind.</p>
-                                <p>You can download the PSD template in this portfolio sample item at <a href="http://freebiesxpress.com/gallery/dreams-free-one-page-web-template/">FreebiesXpress.com</a>.</p>
+                                <h2>Tequila</h2>
+                                <p class="item-intro text-muted">For those looking for a good and bad night</p>
+                                <ul class="list-inline">
+                                    <li><img src="Images/Tequila.jpg" class="img-responsive" alt="">
+                                    <p>Tequila</p></li>
+                                    <li><img src="Images/Tequila.jpg" class="img-responsive" alt="">
+                                    <p>tequila</p></li>
+                                    <li><img src="Images/Tequila.jpg" class="img-responsive" alt="">
+                                    <p>Tequila</p></li>
+                                </ul>
                                 <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
                             </div>
                         </div>
@@ -588,7 +614,7 @@
         $('html,body').animate({scrollTop: $("#").offset().top}, 'slow');
         shiftWindow();
     });
-	
+
 	function display() {
 		var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
@@ -600,8 +626,8 @@
         xmlhttp.open("GET", "testing/Database Testing/testDBh.php?search=" + search, true);
         xmlhttp.send();
 	}
-	
-	
+
+
 </script>
 </body>
 </html>
